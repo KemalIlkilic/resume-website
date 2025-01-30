df02fc6ebe9a733c79a5eeb98cd7cb83127cc9f1
Kemal Onur İlkılıç <kemalonur.ilkilic@bahcesehir.edu.tr>
2025-01-30 18:36:09 +0300

    inital

---
+++ src/app/layout.tsx
@@ -0,0 +1,39 @@
+import { type Metadata } from 'next'
+
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Kemal Onur Ilkilic',
+    default:
+      'Kemal Onur Ilkilic - Software engineer, student, and amateur chess player',
+  },
+  description:
+    'I’m Kemal, a software engineer based in Istanbul. I specialize in building innovative and scalable software solutions. Passionate about problem-solving, I develop technology that enhances everyday life and pushes the boundaries of what’s possible.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
